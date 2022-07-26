81ba32873079923e58ddd88e64c0c6b57eb14847
Paul Heredea <Paul.Heredea@qubiz.com>
2022-07-26 15:06:32 +0300

    New changes

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,11 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+    <button *ngIf="value">{{ value }}</button>
+    <button *ngIf="!value"></button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { width: 100%; height: 100%; font-size: 5em; border: 2px solid black; border-radius: 15px; }'],
 })
 export class SquareComponent  {
 
