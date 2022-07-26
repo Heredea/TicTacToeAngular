f770f0b8c2c87ae106324dcc0a7a84e4380f4b4c
Paul Heredea <Paul.Heredea@qubiz.com>
2022-07-26 16:37:30 +0300

    Change back

--- src/app/board/board.component.spec.ts
+++ src/app/board/board.component.spec.ts
@@ -14,12 +14,71 @@ describe('BoardComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({ 
+      declarations: [ BoardComponent]
+    });
     fixture = TestBed.createComponent(BoardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Verify component variable before ngOnInit', () => {
+    expect(component.squares).toBe([]);
+    expect(component.xIsNext).toBe(false);
+    expect(component.winner).toBe("");
   });
+
+  it('Verify component variable after ngOnInit', () => {
+    let matchingArray = Array(9).fill(null);
+    component.ngOnInit();
+    expect(component.squares).toBe(matchingArray);
+    expect(component.xIsNext).toBe(true);
+    expect(component.winner).toBe(null);
+  });
+
+  it('Verify return values of get player()', () => {
+    component.xIsNext = true;
+    expect(component.player).toBe('X');
+    component.xIsNext = false;
+    expect(component.player).toBe('O');
+  });
+
+  it('Verify return values of get player()', () => {
+    component.xIsNext = true;
+    expect(component.player).toBe('X');
+    component.xIsNext = false;
+    expect(component.player).toBe('O');
+  });
+
+  it('Verify return values of calculateWinner()', () => {
+    let winnCombination = [
+      ['X','O','O'],
+      ['O','X','X'],
+      ['X','O','X'],
+    ];
+
+    let loseCombination = [
+      ['X','O',''],
+      ['O','O','X'],
+      ['X','O','X'],
+    ];
+
+    component.squares = winnCombination;
+
+    expect(component.calculateWinner()).toBe('X');
+
+    component.squares = loseCombination;
+
+    expect(component.calculateWinner()).toBe('O');
+  });
+
+  it('Verify return values of get player()', () => {
+    component.ngOnInit();
+    let boardPosition: number = 3;
+    component.makeMove(boardPosition);
+    expect(component.squares[boardPosition]).toBe('X');
+    expect(component.xIsNext).toBe(false);
+    expect(component.winner).toBe(null);
+  });
+
 });
